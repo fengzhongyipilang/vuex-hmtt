98573452b8281468d9faef38dd81da29a5fa7dd0
ydz <yaodazhen.com>
2020-01-11 15:23:43 +0800

    引入全局样式

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
+
 import 'amfe-flexible' // 引入屏幕适配插件
 import 'vant/lib/index.css'
-
+import '../src/styles/index.less' // 引入全局定义样式
 Vue.config.productionTip = false
 
 Vue.use(Vant)
